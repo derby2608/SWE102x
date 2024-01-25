32e0960d2e1a2e8ee1e1e31d478d5b03dd7a465a
derby2608 <130582820+derby2608@users.noreply.github.com>
2024-01-25 10:54:48 +0700

    Add files via upload

---
+++ SWE102x_Assignment3/src/main/java/com/example/swe102x_assignment3/AddCalculator.java
@@ -0,0 +1,12 @@
+package com.example.swe102x_assignment3;
+
+public class AddCalculator {
+    public int add (int a, int b){
+        return a + b;
+    }
+
+    public int multiply (int a, int b){
+        return a * b;
+    }
+
+}
